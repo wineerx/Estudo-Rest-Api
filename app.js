e1045163df00949a9e3d2f6c74b312046b4f732a
w1nneer <83048827+w1nneer@users.noreply.github.com>
2021-06-18 23:10:53 -0300

    Rotas de Produtos

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/teste', (req, res, next) => {
     res.status(200).send({
-
         mensagem: 'Acesso Liberado!'
     });
 });
